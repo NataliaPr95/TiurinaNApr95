31a7016136b96a759519b9211a4c2940bcd9e7ed
NataliaPr95 <67143402+NataliaPr95@users.noreply.github.com>
2021-03-17 16:16:26 +0700

    Add files via upload

---
+++ tiurinaPR95/TiurinaPr95/I001/l001/I001/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace I001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DG = new System.Windows.Forms.DataGridView();
+            this.update = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.select = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Nam = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DG
+            // 
+            this.DG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DG.Location = new System.Drawing.Point(27, 268);
+            this.DG.Name = "DG";
+            this.DG.Size = new System.Drawing.Size(537, 137);
+            this.DG.TabIndex = 23;
+            this.DG.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DG_CellClick);
+            // 
+            // update
+            // 
+            this.update.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.update.Location = new System.Drawing.Point(413, 81);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(105, 32);
+            this.update.TabIndex = 22;
+            this.update.Text = "Обновить";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Add.Location = new System.Drawing.Point(413, 25);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(105, 39);
+            this.Add.TabIndex = 21;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Delete.Location = new System.Drawing.Point(290, 81);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(100, 32);
+            this.Delete.TabIndex = 20;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // select
+            // 
+            this.select.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.select.Location = new System.Drawing.Point(290, 25);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(100, 39);
+            this.select.TabIndex = 19;
+            this.select.Text = "Просмотр";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(23, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 19);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Введите имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Введите логин:";
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.id.Location = new System.Drawing.Point(26, 30);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(186, 26);
+            this.id.TabIndex = 15;
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login.Location = new System.Drawing.Point(27, 132);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(186, 26);
+            this.login.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Введите id :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(23, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 19);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Введите пароль:";
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pass.Location = new System.Drawing.Point(27, 183);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(186, 26);
+            this.pass.TabIndex = 24;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(27, 229);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(200, 23);
+            this.checkBox1.TabIndex = 28;
+            this.checkBox1.Text = "Администратор или нет?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Nam
+            // 
+            this.Nam.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Nam.Location = new System.Drawing.Point(27, 81);
+            this.Nam.Name = "Nam";
+            this.Nam.Size = new System.Drawing.Size(186, 26);
+            this.Nam.TabIndex = 29;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(332, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Очистка ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Nam);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.DG);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DG;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox Nam;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ tiurinaPR95/TiurinaPr95/I001/l001/I001/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace I001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void GetTable()
+        {
+            string constr = @"Data Source=.\SQLEXPRESS;Initial Catalog=I001;User ID=student;Password=Passw0rd";
+            SqlConnection conn = new SqlConnection(constr);
+            SqlCommand com = new SqlCommand("SELECT * FROM Users", conn);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+            SqlDataReader sdr = com.ExecuteReader();
+            dt.Load(sdr);
+            conn.Close();
+
+            DG.DataSource = dt;
+        }
+        private void select_Click(object sender, EventArgs e)
+        {
+            GetTable();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (id.Text != "" && Nam.Text != "" && login.Text != "" && pass.Text != "")
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=I001;User ID=student;Password=Passw0rd"))//строка подключения
+                {
+                    try
+                    {
+                        con.Open();
+                        SqlCommand com = con.CreateCommand();
+                        com.CommandText = $"INSERT INTO Users (id, name, login, password, isadmin) VALUES('" + id.Text + "','" + Nam.Text + "','" + login.Text + "','" + pass.Text + "', @adm)";
+                        if (checkBox1.Checked == true)
+                        {
+                            com.Parameters.AddWithValue("@adm", checkBox1.Checked == true);
+
+                        }
+                        else
+                        {
+                            com.Parameters.AddWithValue("@adm", checkBox1.Checked == false);
+                        }
+                        com.ExecuteNonQuery();
+                        GetTable();//обновление таблицы
+                        MessageBox.Show("Добавить удалось!");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        MessageBox.Show("Добавить не удалось!");
+
+                    }
+                    con.Close();
+                }
+            }
+            else
+                MessageBox.Show("Введите данные!!!");
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Вы уверены, что хотите удалить?", "Удалить", MessageBoxButtons.YesNo);//сообщение для подтверждения удаления
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (id.Text != "")
+                    using (SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=I001;User ID=student;Password=Passw0rd"))//строка подключения
+                    {
+                        try
+                        {
+                            con.Open();
+                            SqlCommand com = con.CreateCommand();
+                            com.CommandText = $"DELETE FROM Users WHERE id = @id";//запрос удаления 
+                            com.Parameters.AddWithValue(@"id", id.Text);
+                            com.ExecuteNonQuery();
+                            GetTable();//обновление таблицы
+                            MessageBox.Show("Запись удалена");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                        con.Close();
+                    }
+                else
+                    MessageBox.Show("Введите данные!!");
+                clear(); 
+            }
+            else 
+            if (dialogResult == DialogResult.No)
+            {
+                clear();
+            }
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            if (id.Text != "" && Nam.Text != "" && login.Text != "" && pass.Text != "")
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=I001;User ID=student;Password=Passw0rd"))//строка подключения
+                {
+                    try
+                    {
+                        con.Open();
+                        SqlCommand com = con.CreateCommand();
+                        //Обновление информации
+                        com.CommandText = $"Update Users SET id='{id.Text}', name='{Nam.Text}', login='{login.Text}', password='{pass.Text}', isadmin=@adm Where id='{id.Text}'";
+                        if (checkBox1.Checked == true)
+                        {
+                            com.Parameters.AddWithValue("@adm", checkBox1.Checked == true);
+
+                        }
+                        else
+                        {
+                            com.Parameters.AddWithValue("@adm", checkBox1.Checked == false);
+                        }
+                        com.ExecuteNonQuery();
+                        GetTable();//обновление таблицы
+                        MessageBox.Show("Запись обновлена");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        MessageBox.Show("Обновить запись не удалось!");
+                    }
+                    
+                    con.Close();
+                }
+            }
+            else
+                MessageBox.Show("Введите данные!!!");
+        }
+
+        private void DG_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id.Text = DG.CurrentRow.Cells[0].Value.ToString();
+            Nam.Text = DG.CurrentRow.Cells[1].Value.ToString();
+            login.Text = DG.CurrentRow.Cells[2].Value.ToString();
+            pass.Text = DG.CurrentRow.Cells[3].Value.ToString();
+            if (DG.CurrentRow.Cells[4].Value.ToString() == "" && DG.CurrentRow.Cells[3].Value.ToString()=="" && DG.CurrentRow.Cells[2].Value.ToString()=="" && DG.CurrentRow.Cells[1].Value.ToString() == "" && DG.CurrentRow.Cells[0].Value.ToString() == "") {
+                id.Clear();
+                Nam.Clear();
+                login.Clear();
+                pass.Clear();
+                checkBox1.Checked = false;
+            }
+            else
+            if((bool)(DG.CurrentRow.Cells[4].Value) == true){
+                checkBox1.Checked = true;
+            }
+            else
+                checkBox1.Checked = false;
+        }
+
+        public void clear()
+        {
+            id.Clear();
+            Nam.Clear();
+            login.Clear();
+            pass.Clear();
+            checkBox1.Checked = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+    }
+    }
+
